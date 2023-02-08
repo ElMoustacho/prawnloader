c6d777864394615cb20e13fdda040460ffcf8aa6
Valentin Bonetti <valentin.bonetti@viacesi.fr>
2023-02-08 20:13:56 +0100

    Add queue

--- src-tauri/src/downloader.rs
+++ src-tauri/src/downloader.rs
@@ -6,15 +6,38 @@ pub trait DownloadableSong {
     fn get_song(&self) -> &Song;
 }
 
-pub fn download(
-    url: impl Into<String>,
-    dest_folder: &Path,
-) -> Option<(Box<dyn DownloadableSong>, Box<Path>)> {
-    let downloadable = parse_url(&url.into())?;
+pub struct Downloader {
+    queue: Vec<Box<dyn DownloadableSong>>,
+}
 
-    if let Ok(dest_file) = downloadable.as_ref().download(dest_folder) {
-        return Some((downloadable, dest_file));
+impl Downloader {
+    pub fn new() -> Downloader {
+        Downloader { queue: vec![] }
     }
 
-    None
+    pub fn add_to_queue(&mut self, url: impl Into<String>) -> Result<(), ()> {
+        let mut downloadables = parse_url(&url.into())?;
+
+        self.queue.append(&mut downloadables);
+
+        Ok(())
+    }
+
+    pub fn remove_from_queue(&mut self, index: usize) -> Result<(), ()> {
+        if self.queue.len() <= index {
+            return Err(());
+        }
+
+        self.queue.remove(index);
+
+        Ok(())
+    }
+
+    pub fn download(&self, index: usize, dest_folder: &Path) -> Result<Box<Path>, ()> {
+        if let Some(downloadable) = self.queue.get(index) {
+            return downloadable.download(dest_folder);
+        }
+
+        Err(())
+    }
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,32 +4,26 @@
 )]
 
 use prawnloader::downloader;
-use std::path::Path;
-use tauri::api::path::download_dir;
 
 fn main() {
-    struct MockOptions<'a> {
-        pub dest_folder: &'a Path,
-    }
-
-    let mock_options = MockOptions {
-        dest_folder: &download_dir().unwrap(),
-    };
+    let mut downloader = downloader::Downloader::new();
 
     let urls = vec![
-        // "https://www.deezer.com/fr/track/597403742",
-        // "https://deezer.page.link/mZsk7WU6P4r4h3nA8",
-        // "https://www.deezer.com/fr/album/345755977",
-        // "https://www.deezer.com/fr/playlist/10575085742",
-        // "https://music.youtube.com/watch?v=gAy5WZo9kts",
-        // "https://music.youtube.com/playlist?list=OLAK5uy_nSewatBUjTf3IO_DIqqMXn3ps_WbEAyi4",
+        "https://www.deezer.com/fr/track/597403742",
+        "https://deezer.page.link/mZsk7WU6P4r4h3nA8",
+        "https://www.deezer.com/fr/album/345755977",
+        "https://www.deezer.com/fr/playlist/10575085742",
+        "https://music.youtube.com/watch?v=gAy5WZo9kts",
+        "https://music.youtube.com/playlist?list=OLAK5uy_nSewatBUjTf3IO_DIqqMXn3ps_WbEAyi4",
         "https://www.youtube.com/watch?v=ORofRTMg-iY",
-        // "https://www.youtube.com/playlist?list=PLevurNKwl9HEcxa6K3dUoQ1jSBUUC2UxI",
+        "https://www.youtube.com/playlist?list=PLevurNKwl9HEcxa6K3dUoQ1jSBUUC2UxI",
     ];
 
     for url in urls {
-        if let Some(t) = downloader::download(url, mock_options.dest_folder) {
-            println!("Downloaded {}", t.0.as_ref().get_song().title);
+        if let Ok(_) = downloader.add_to_queue(url) {
+            println!("Added \"{}\" to queue", url);
+        } else {
+            println!("Error adding \"{}\" to queue.", url);
         }
     }
 
