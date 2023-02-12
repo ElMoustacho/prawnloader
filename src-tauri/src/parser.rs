ae3f5f022b33fa0438669c2b2d4ac2ae8716cea1
Valentin Bonetti <valentin.bonetti@viacesi.fr>
2023-02-12 14:55:03 +0100

    Clean up Parser & add docs

--- src-tauri/src/parser.rs
+++ src-tauri/src/parser.rs
@@ -1,20 +1,37 @@
 use crate::{downloader::DownloadableSong, youtube::YoutubeParser};
-use tokio::sync::Mutex;
 
 pub type ParserResult = Result<Vec<Box<dyn DownloadableSong>>, ()>;
 
+/**
+A parser used to transform URLs into downloadable songs with metadata.
+ */
 pub trait SongParser: Send {
     fn parse_url(&self, url: &String) -> ParserResult;
 }
 
+/**
+Manages multiple parsers and returns the first successful parse, else `Err`.
+ */
 pub struct Parser {
-    parsers: Mutex<Vec<Box<dyn SongParser>>>,
+    pub parsers: Vec<Box<dyn SongParser>>,
 }
 
 impl Parser {
+    /**
+    Create a new `Parser` with a default list of parsers.
+    */
     pub fn new() -> Parser {
         Parser {
-            parsers: Mutex::new(vec![Box::new(YoutubeParser {})]),
+            parsers: vec![Box::new(YoutubeParser {})],
+        }
+    }
+
+    /**
+    Create a new `Parser` with a custom list of parsers.
+    */
+    pub fn from(parsers: Vec<Box<dyn SongParser>>) -> Parser {
+        Parser {
+            parsers: Vec::from(parsers),
         }
     }
 
@@ -26,9 +43,7 @@ impl Parser {
     This function will return an error if the url passed doesn't match with any parser.
     */
     pub(crate) fn parse_url(&self, url: &String) -> ParserResult {
-        let parsers = tokio::task::block_in_place(|| self.parsers.blocking_lock());
-
-        for parser in parsers.iter() {
+        for parser in self.parsers.iter() {
             if let Ok(downloadable_list) = parser.parse_url(&url) {
                 return Ok(downloadable_list);
             }
@@ -36,44 +51,34 @@ impl Parser {
 
         Err(())
     }
-
-    pub fn add_parser(&self, parser: Box<dyn SongParser>) {
-        tokio::task::block_in_place(|| self.parsers.blocking_lock().push(parser));
-    }
 }
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn video_parse() {
+    fn test_default_parsers() {
         let parser = Parser::new();
 
-        parser
-            .parse_url(&String::from(
-                "https://music.youtube.com/watch?v=gAy5WZo9kts",
-            ))
-            .expect("Url should be parsable to a YT video.");
+        let urls = [
+            // Deezer
+            "https://www.deezer.com/fr/track/597403742",
+            "https://deezer.page.link/mZsk7WU6P4r4h3nA8",
+            "https://www.deezer.com/fr/album/345755977",
+            "https://www.deezer.com/fr/playlist/10575085742",
+            // YouTube Music
+            "https://music.youtube.com/watch?v=gAy5WZo9kts",
+            "https://music.youtube.com/playlist?list=OLAK5uy_nSewatBUjTf3IO_DIqqMXn3ps_WbEAyi4",
+            // YouTube
+            "https://www.youtube.com/watch?v=ORofRTMg-iY",
+            "https://www.youtube.com/playlist?list=PLevurNKwl9HEcxa6K3dUoQ1jSBUUC2UxI",
+        ];
 
-        parser
-            .parse_url(&String::from("https://www.youtube.com/watch?v=ORofRTMg-iY"))
-            .expect("Url should be parsable to a YT video, converted from a YT music URL.");
-    }
-
-    #[test]
-    fn playlist_parse() {
-        let parser = Parser::new();
-
-        parser
-            .parse_url(&String::from(
-                "https://music.youtube.com/playlist?list=OLAK5uy_nSewatBUjTf3IO_DIqqMXn3ps_WbEAyi4",
-            ))
-            .expect("Url should be parsable to a YT playlist.");
-
-        parser
-            .parse_url(&String::from(
-                "https://www.youtube.com/playlist?list=PLevurNKwl9HEcxa6K3dUoQ1jSBUUC2UxI",
-            ))
-            .expect("Url should be parsable to a YT playlist, converted from a YT music URL.");
+        for url in urls {
+            parser
+                .parse_url(&url.to_string())
+                .expect(&format!("Url '{}' should be parsable.", url)[..]);
+        }
     }
 }
