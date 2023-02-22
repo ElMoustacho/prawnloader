7aee61231ad74104151951e1841270279349930a
Valentin Bonetti <valentin.bonetti@viacesi.fr>
2023-02-22 19:10:10 +0100

    Remove tests from parser

--- src-tauri/src/parser.rs
+++ src-tauri/src/parser.rs
@@ -1,7 +1,8 @@
 use crate::{downloader::DownloadableSong, youtube::YoutubeParser};
+use anyhow::{bail, Result};
 use async_trait::async_trait;
 
-pub type ParserResult = Result<Vec<Box<dyn DownloadableSong>>, ()>;
+pub type ParserResult = Result<Vec<Box<dyn DownloadableSong>>>;
 
 /**
 A parser used to transform URLs into downloadable songs with metadata.
@@ -24,7 +25,7 @@ impl Parser {
     */
     pub fn new() -> Parser {
         Parser {
-            parsers: vec![Box::new(YoutubeParser {})],
+            parsers: vec![Box::new(YoutubeParser::new())],
         }
     }
 
@@ -51,37 +52,6 @@ impl Parser {
             }
         }
 
-        Err(())
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[tokio::test]
-    async fn test_default_parsers() {
-        let parser = Parser::new();
-
-        let urls = [
-            // Deezer
-            "https://www.deezer.com/fr/track/597403742",
-            "https://deezer.page.link/mZsk7WU6P4r4h3nA8",
-            "https://www.deezer.com/fr/album/345755977",
-            "https://www.deezer.com/fr/playlist/10575085742",
-            // YouTube Music
-            "https://music.youtube.com/watch?v=gAy5WZo9kts",
-            "https://music.youtube.com/playlist?list=OLAK5uy_nSewatBUjTf3IO_DIqqMXn3ps_WbEAyi4",
-            // YouTube
-            "https://www.youtube.com/watch?v=ORofRTMg-iY",
-            "https://www.youtube.com/playlist?list=PLevurNKwl9HEcxa6K3dUoQ1jSBUUC2UxI",
-        ];
-
-        for url in urls {
-            parser
-                .parse_url(&url.to_string())
-                .await
-                .expect(&format!("Url '{}' should be parsable.", url)[..]);
-        }
+        bail!("")
     }
 }
