fd491dfc43b335b95aaba9f73b5b9a588e760431
ElMoustacho <pikachu2154@gmail.com>
2023-11-28 21:29:04 +0100

    Move logs to own file

---
+++ src/lib/ts/log.ts
@@ -0,0 +1,42 @@
+import type { Song } from 'src/types/music';
+import { writable, type Writable } from 'svelte/store';
+
+export class Log {
+	success: boolean;
+	content: string;
+
+	constructor(success: boolean, content: string) {
+		this.success = success;
+		this.content = content;
+	}
+}
+
+export const logs: Writable<Log[]> = writable([]);
+
+export function addLog(log: Log) {
+	logs.update(logs => {
+		logs.push(log);
+		return logs;
+	});
+}
+
+export function clearLogs() {
+	// TODO
+	throw new Error('Not implemented');
+}
+
+export function formatLogSuccess(song: Song): Log {
+	return new Log(true, `Downloaded ${song.artist.name} - ${song.title}`);
+}
+
+export function formatLogAlbumNotFound(albumId: number): Log {
+	return new Log(false, `Album ${albumId} not found`);
+}
+
+export function formatLogSongNotFound(songId: number): Log {
+	return new Log(false, `Song ${songId} not found`);
+}
+
+export function formatLogDownloadError(song: Song): Log {
+	return new Log(false, `Error while downloading ${song.artist.name} - ${song.title}`);
+}
