ed7efd5aea9f74f6fa3f018e4f72f11038d9bf9d
ElMoustacho <pikachu2154@gmail.com>
2023-12-05 15:59:53 +0100

    Update Event

--- src-tauri/src/events.rs
+++ src-tauri/src/events.rs
@@ -6,7 +6,21 @@ use crate::{downloader::ProgressEvent, models::music::Song};
 #[derive(TS, Serialize)]
 #[ts(export, export_to = "../src/events.ts")]
 pub enum Event {
-    ProgressEvent(ProgressEvent),
+    Waiting(Song),
+    Start(Song),
+    Finish(Song),
+    DownloadError(Song),
     AddToQueue(Song),
     RemoveFromQueue(Song),
+}
+
+impl From<ProgressEvent> for Event {
+    fn from(event: ProgressEvent) -> Self {
+        match event {
+            ProgressEvent::Waiting(track) => Self::Waiting(Song::from(track)),
+            ProgressEvent::Start(track) => Self::Start(Song::from(track)),
+            ProgressEvent::Finish(track) => Self::Finish(Song::from(track)),
+            ProgressEvent::DownloadError(track) => Self::DownloadError(Song::from(track)),
+        }
+    }
 }
