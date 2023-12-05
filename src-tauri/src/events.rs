afa7acca7073ba6ddd8d73b7d58130720a1dabf6
ElMoustacho <pikachu2154@gmail.com>
2023-12-05 15:57:06 +0100

    Derive TS for Event

--- src-tauri/src/events.rs
+++ src-tauri/src/events.rs
@@ -1,5 +1,10 @@
+use serde::Serialize;
+use ts_rs::TS;
+
 use crate::{downloader::ProgressEvent, models::music::Song};
 
+#[derive(TS, Serialize)]
+#[ts(export, export_to = "../src/events.ts")]
 pub enum Event {
     ProgressEvent(ProgressEvent),
     AddToQueue(Song),
