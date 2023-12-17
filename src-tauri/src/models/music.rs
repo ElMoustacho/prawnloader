738a386b75c965cd4cb98318d518fc07f9cbdccf
ElMoustacho <pikachu2154@gmail.com>
2023-12-17 20:59:51 +0100

    Add source to song

--- src-tauri/src/models/music.rs
+++ src-tauri/src/models/music.rs
@@ -1,17 +1,26 @@
 use deezer::models::Track;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use ts_rs::TS;
 
-#[derive(TS, Debug, Serialize, Clone, Default)]
+#[derive(TS, Debug, Serialize, Deserialize, Clone)]
 #[ts(export, export_to = "../src/models/")]
 pub struct Album {
     pub title: String,
     pub cover_url: String,
 }
 
-#[derive(TS, Debug, Serialize, Clone, Default)]
+#[derive(TS, Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "lowercase")]
+enum Downloader {
+    Youtube,
+    Deezer,
+}
+
+#[derive(TS, Debug, Serialize, Deserialize, Clone)]
 #[ts(export, export_to = "../src/models/")]
 pub struct Song {
+    #[ts(inline)]
+    source: Downloader,
     pub id: String,
     pub title: String,
     pub album: Album,
@@ -21,6 +30,7 @@ pub struct Song {
 impl From<Track> for Song {
     fn from(track: Track) -> Self {
         Self {
+            source: Downloader::Deezer,
             id: track.id.to_string(),
             title: track.title,
             artist: track.artist.name,
@@ -35,9 +45,13 @@ impl From<Track> for Song {
 impl From<rusty_ytdl::search::Video> for Song {
     fn from(video: rusty_ytdl::search::Video) -> Self {
         Self {
+            source: Downloader::Youtube,
             id: video.id,
             title: video.title,
-            album: Album::default(),
+            album: Album {
+                title: String::new(),
+                cover_url: String::new(),
+            },
             artist: video.channel.name,
         }
     }
@@ -50,15 +64,16 @@ impl From<rusty_ytdl::VideoDetails> for Song {
             .map(|author| author.name)
             .unwrap_or_default();
         let album = Album {
+            title: String::new(),
             cover_url: video_details
                 .thumbnails
                 .first()
                 .map(|t| t.url.clone())
                 .unwrap_or_default(),
-            ..Default::default()
         };
 
         Self {
+            source: Downloader::Youtube,
             id: video_details.video_id,
             title: video_details.title,
             album,
