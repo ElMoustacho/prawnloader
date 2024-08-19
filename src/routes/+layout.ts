4b311cc8a8e392bfb960d776a6ecd58b1108de9b
ElMoustacho <pikachu2154@gmail.com>
2024-08-19 13:07:31 +0200

    Load config on app start

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,9 @@
+import { invoke } from '$lib/tauri-wrapper';
+
 export const prerender = true;
 export const ssr = false;
+
+/** @type {import('./$types').PageLoad} */
+export async function load() {
+	return { config: invoke('get_config', {}) };
+}
