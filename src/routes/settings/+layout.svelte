7435740e3fb476e5e81d173d96e8ca1e6ce625db
ElMoustacho <pikachu2154@gmail.com>
2024-08-19 13:05:38 +0200

    Add Deezer / Youtube config pages

--- src/routes/settings/+layout.svelte
+++ src/routes/settings/+layout.svelte
@@ -3,9 +3,11 @@
 <div class="columns">
 	<aside class="column menu is-3">
 		<ul class="menu-list">
-			<a class="navbar-item" href="/settings">General</a>
+			<a class="navbar-item" href="/settings"><i class="fa-solid fa-gear"></i> General</a>
 
-			<a class="navbar-item" href="downloader">Downloader</a>
+			<a class="navbar-item" href="youtube"><i class="fa-brands fa-youtube"></i> Youtube</a>
+
+			<a class="navbar-item" href="deezer"><i class="fa-brands fa-deezer"></i> Deezer</a>
 		</ul>
 	</aside>
 
---
+++ src/routes/settings/youtube/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { Config } from '$models/Config';
+	import type { UnionToTuple } from 'src/union-to-tuple';
+	import type { PageData } from '../downloader/$types';
+
+	export let data: PageData;
+
+	const youtubeFormats: UnionToTuple<Config['youtubeFormat']> = ['MP3', 'WEBM', 'WAV', 'OGG'];
+</script>
+
+<h2 class="subtitle">Youtube</h2>
+
+<label>
+	Music format:
+	<select value={data.config.youtubeFormat}>
+		{#each youtubeFormats as youtubeFormat}
+			<option value={youtubeFormat}>{youtubeFormat}</option>
+		{/each}
+	</select>
+</label>
