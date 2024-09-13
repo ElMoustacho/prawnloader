a5d974968948cf62869b01f8fdfb0458184e6e70
ElMoustacho <pikachu2154@gmail.com>
2024-09-14 00:23:02 +0200

    Add placeholder queue_items components

--- src/lib/components/queue_items/DeezerTrack.svelte
+++ src/lib/components/queue_items/DeezerTrack.svelte
@@ -1,30 +1,30 @@
 <script lang="ts">
+	import type { QueueItem } from '$lib/music';
 	import { queue } from '$lib/stores';
 	import { invoke } from '$lib/tauri-wrapper';
-	import type { QueueSong } from '../music';
 
-	export let queueSong: QueueSong;
+	export let queueItem: QueueItem;
 
-	$: downloading = queueSong.download_state === 'Downloading';
+	$: downloading = queueItem.download_state === 'Downloading';
 </script>
 
 <div class="list-item p-2">
 	<div class="list-item-image">
 		<figure class="image is-32x32">
-			<img src={queueSong.song.album.cover_url} alt="" />
+			<img src={queueItem.song.album.cover_url} alt="" />
 		</figure>
 	</div>
 
 	<div class="list-item-content">
-		<div class="list-item-title" title={queueSong.song.title}>
-			<span>[<b>{queueSong.download_state}</b>]</span>
-			<span>{queueSong.song.title}</span>
+		<div class="list-item-title" title={queueItem.song.title}>
+			<span>[<b>{queueItem.download_state}</b>]</span>
+			<span>{queueItem.song.title}</span>
 		</div>
 		<div class="list-item-description">
 			<div class="is-flex is-justify-content-space-between">
-				<span title={queueSong.song.album.title}>{queueSong.song.album.title}</span>
-				<span title={queueSong.song.artist} class="is-single-line has-text-black-bis"
-					>{queueSong.song.artist}</span>
+				<span title={queueItem.song.album.title}>{queueItem.song.album.title}</span>
+				<span title={queueItem.song.artist} class="is-single-line has-text-black-bis"
+					>{queueItem.song.artist}</span>
 			</div>
 		</div>
 	</div>
@@ -33,7 +33,7 @@
 		<div class="buttons is-right">
 			<button
 				class="button"
-				on:click={() => invoke('request_download', { song: queueSong.song })}
+				on:click={() => invoke('request_download', { song: queueItem.song })}
 				disabled={downloading}>
 				<span class="icon is-small">
 					<i class="fas fa-download" />
@@ -42,7 +42,7 @@
 			<button
 				class="button is-danger"
 				on:click={() =>
-					queue.update(queue => queue.filter(x => x.song.id !== queueSong.song.id))}
+					queue.update(queue => queue.filter(x => x.song.id !== queueItem.song.id))}
 				disabled={downloading}>
 				<span class="icon is-small">
 					<i class="fas fa-trash" />
---
+++ src/lib/components/queue_items/YoutubePlaylist.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import type { QueueItem } from '$lib/music';
+
+	export let queueItem: QueueItem;
+</script>
+
+<h1>😮 No content!</h1>
