321f115098d634b6156cbf9cc921a04e3d91cc26
ElMoustacho <pikachu2154@gmail.com>
2024-09-17 18:47:13 +0200

    Implement DeezerAlbum

--- src/lib/components/queue_items/DeezerAlbum.svelte
+++ src/lib/components/queue_items/DeezerAlbum.svelte
@@ -1,7 +1,60 @@
 <script lang="ts">
 	import type { QueueItem } from '$lib/music';
+	import { queue } from '$lib/stores';
+	import { invoke } from '$lib/tauri-wrapper';
 
 	export let queueItem: QueueItem;
+
+	if (queueItem.item.type !== 'DeezerAlbum')
+		throw new Error('Item should be of type DeezerAlbum.');
+
+	const { album } = queueItem.item;
+
+	$: downloading = queueItem.download_status === 'Downloading';
 </script>
 
-<h1>😮 No content!</h1>
+<div class="list-item p-2">
+	<div class="list-item-image">
+		<figure class="image is-32x32">
+			<img src={album.cover_url} alt="" />
+		</figure>
+	</div>
+
+	<div class="list-item-content">
+		<div class="list-item-title" title={album.title}>
+			<span>[<b>{queueItem.download_status}</b>]</span>
+			<span>{album.title}</span>
+		</div>
+		<div class="list-item-description">
+			<div class="is-flex is-justify-content-space-between">
+				<div>
+					<span><b>Album</b> | {album.songs.length} tracks</span>
+				</div>
+				<span title={album.artist} class="is-single-line has-text-black-bis"
+					>{album.artist}</span>
+			</div>
+		</div>
+	</div>
+
+	<div class="list-item-controls">
+		<div class="buttons is-right">
+			<button
+				class="button"
+				on:click={() => invoke('request_download', { request: queueItem })}
+				disabled={downloading}>
+				<span class="icon is-small">
+					<i class="fas fa-download" />
+				</span>
+			</button>
+			<button
+				class="button is-danger"
+				on:click={() =>
+					queue.update(queue => queue.filter(x => x.request_id === queueItem.request_id))}
+				disabled={downloading}>
+				<span class="icon is-small">
+					<i class="fas fa-trash" />
+				</span>
+			</button>
+		</div>
+	</div>
+</div>
