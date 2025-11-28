49b6b1e8b1b01720738c54d9e44661e28a791e75
ElMoustacho <pikachu2154@gmail.com>
2025-11-28 10:19:59 +0100

    Add postprocess

--- src-tauri/src/downloaders/ytdlp.rs
+++ src-tauri/src/downloaders/ytdlp.rs
@@ -4,36 +4,58 @@ use color_eyre::eyre::Result;
 use tauri::api::path::download_dir;
 use yt_dlp::{
     client::deps::{Libraries, LibraryInstaller},
+    download::{AudioCodec, PostProcessConfig},
     Youtube,
 };
 
 use crate::downloaders::ytdlp;
-
-async fn download() -> Result<()> {
-    let libraries_dir = PathBuf::from("libs");
-    let output_dir = download_dir().unwrap();
-
-    let youtube = Youtube::with_new_binaries(libraries_dir, output_dir)
-        .await
-        .expect("Failed to create a new Youtube instance");
-    youtube
-        .download_audio_stream_with_quality(
-            "bbcPLei01Ls",
-            "testfile.mp3",
-            yt_dlp::model::AudioQuality::Best,
-            yt_dlp::model::AudioCodecPreference::MP3,
-        )
-        .await
-        .inspect(|_| println!("Downloaded successfully"))?;
-
-    Ok(())
-}
 
 #[cfg(test)]
 mod tests {
     use tracing::Level;
 
     use super::*;
+
+    async fn download() -> Result<()> {
+        let libraries_dir = PathBuf::from("libs");
+        let output_dir = download_dir().unwrap();
+
+        let youtube = Youtube::with_new_binaries(libraries_dir, output_dir)
+            .await
+            .expect("Failed to create a new Youtube instance");
+        let result = youtube
+            .download_audio_stream_with_quality(
+                "bbcPLei01Ls",
+                "testfile.mp3",
+                yt_dlp::model::AudioQuality::Best,
+                yt_dlp::model::AudioCodecPreference::MP3,
+            )
+            .await
+            .inspect(|_| println!("Downloaded successfully"))?;
+
+        let r = result.with_file_name("prout.mp3");
+
+        let post_process_result = youtube
+            .postprocess_video(
+                result,
+                r.to_str().unwrap(),
+                PostProcessConfig {
+                    video_codec: None,
+                    audio_codec: Some(AudioCodec::MP3),
+                    video_bitrate: None,
+                    audio_bitrate: None,
+                    resolution: None,
+                    framerate: None,
+                    preset: None,
+                    filters: Vec::new(),
+                },
+            )
+            .await;
+
+        tracing::info!("Post process result: {post_process_result:#?}");
+
+        Ok(())
+    }
 
     #[tokio::test]
     async fn ytdlp() {
